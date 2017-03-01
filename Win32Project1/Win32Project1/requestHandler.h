fc892dbd17af6da5ea6698a8cf79ccb3e2c0b398
Jakob Løver <jakob.lover@ebnett.no>
2017-03-01 16:21:17 +0100

    removed visual project

--- Win32Project1/Win32Project1/requestHandler.h
+++
@@ -1,3 +0,0 @@
-#include <stdbool.h>
-
-typedef enum {etg1, etg2, etg3, etg4} Etasje;  //enum for å beskrive etasje
--- Win32Project1/Win32Project1/systemReport.h
+++
@@ -1,4 +0,0 @@
-#include <time.h>
-#include <stdio.h>
-
-void printSystemMessage(char *moduleName, char *message);
