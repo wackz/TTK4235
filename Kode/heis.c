ddacb692aec475150f99125f170df05e7dc14aab
Unknown <jostlowe@gmail.com>
2017-03-08 18:36:55 +0100

    Added main code for FSM

--- Kode/heis.c
+++ Kode/heis.c
@@ -15,14 +15,107 @@ void mainElevatorLoop(void){
 	//hovedprogramloop
 	while (true)
 	{
+		//update all elevatorStates
+		updateEmergencyStates();
+		updateCurrentFloor();
+		updateTargetFloor();
+		updateTimerInProgress();
 		
-		//update emergency states
+		//update elevator output
+		updateElevatorOutput();
+
+	}
+}
+
+
+//---------------------------------------------------
+//update functions for state of elevator
+//---------------------------------------------------
+
+static void updateEmergencyStates(void){
 		elevatorState.obstruction = (bool)elev_get_obstruction_signal();
 		elevatorState.emergencyButton = (bool)elev_get_stop_signal();
-		
-		//update currentFloor
+}
+
+static void updateCurrentFloor(void){
 		if(elev_get_floor_sensor_signal() != -1){
 			elevatorState.currentFloor = elev_get_floor_sensor_signal()
+			elevatorState.isAtFloor = true;
+		}
+		else{
+			elevatorState.isAtFloor = false;
+		}
+}
+
+static void updateTargetFloor(void){
+			elevatorState.targetFloor = req_getPrioritizedRequest();
+			if(elevatorState.targetFloor == NONE){
+				elevatorState.idle == true;
+			}
+			else{
+				elevatorState.idle == false;
+			}
+}
+
+//---------------------------------------------------
+//update function for elevator output
+//---------------------------------------------------
+
+static void updateElevatorOutput(void){
+	
+	//close door if timer is done
+	if(tmr_getTimerDone()){
+		elevatorState.doorOpen = false;
+		elev_set_door_open_lamp(0);
+	}
+	
+	
+	
+	// handle emergency button press
+	if(elevatorState.emergencyButton){
+		elev_set_motor_direction(DIRN_STOP); 
+		//åpne døra hvis heisen er ved en etasje
+		if(isAtFloor == true){
+			elevatorState.doorOpen = 1;
+		}
+		req_wipeRequests();
+		elevatorState.targetFloor = NONE;
+	}
+	
+	
+	
+	//handle obstruction
+	else if(elevatorState.obstruction && isAtFloor){
+		tmr_startTimer(3);
+		elevatorState.doorOpen = 1;
+		elev_set_motor_direction(DIRN_STOP); 
+	}
+	
+	
+	
+	//handle open door
+	else if(elevatorState.doorOpen && !elevatorState.idle){
+		elev_set_door_open_lamp(1);
+	} 
+	
+	
+	//handle NONE request
+	else if(elevatorState.idle){
+		elevatorState.doorOpen = 1;
+		elev_set_door_open_lamp(1);
+		elev_set_motor_direction(DIRN_STOP);
+	}
+	
+	
+	//handle other request
+	else if(!elevatorState.idle){
+		elevatorState.doorOpen = 0;
+		elev_set_door_open_lamp(0);
+		if(elevatorState.targetFloor > elevatorState.currentFloor){
+			elev_set_motor_direction(DIRN_UP);
+		}
+		else if(elevatorState.targetFloor < elevatorState.currentFloor){
+			elev_set_motor_direction(DIRN_DOWN);
 		}
 	}
 }
--- Kode/heis.h
+++ Kode/heis.h
@@ -9,12 +9,22 @@
 typedef struct{
 	Etasje currentFloor;
 	Etasje targetFloor;
+	bool isAtFloor;
 	bool direction; // 1 for opp - 0 for ned
 	bool doorOpen; // 1 for åpen - 0 for lukket
 	bool emergencyButton; //1 for trykket - 0 ellers
 	bool obstruction; //1 for obstruksjon på - 0 ellers
+	bool idle;
 } ElevatorState;
 
-static ElevatorState elevatorState = {etg1, 0, 0, 0, 0}; //heisens overordnede tilstand
+static ElevatorState elevatorState = {ETG1, NONE, 1, 1, 1, 0, 0, 1}; //heisens overordnede tilstand
 
 void mainElevatorLoop(void);
+
+//update functions 
+static void updateEmergencyStates(void);
+static void updateCurrentFloor(void);
+static void updateTargetFloor(void);
+static void updateElevatorOutput(void){
+	
+}
